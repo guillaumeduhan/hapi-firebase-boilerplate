377916bb07be7d5dba8b215e2ff746925b519e22
halma <myhappyagency@gmail.com>
2019-12-16 16:28:18 +0100

    firebase-admin instead of firebase

--- services/resources.js
+++ services/resources.js
@@ -4,25 +4,17 @@
 
 'use strict'
 
-const firebase = require('firebase')
-// const chalk = require('chalk')
+const admin = require('firebase-admin')
+const serviceAccount = require('../serviceAccountKey.json')
+const chalk = require('chalk')
 
 exports.initFirebase = () => {
   /*
      * Init your firebase app
      */
-  const firebaseConfig = {
-    apiKey: process.env.VUE_APP_API_KEY,
-    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
-    databaseURL: process.env.VUE_APP_DB_URL,
-    projectId: process.env.VUE_APP_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
-    appId: process.env.VUE_APP_APP_ID,
-  }
-
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig)
-  }
-  // console.log(chalk.bold(chalk.green('✅ Firebase')))
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://test-f9e7a.firebaseio.com',
+  })
+  console.log(chalk.bold(chalk.green('✅ Firebase')))
 }
