95b40a6f3b1daa1441e6c3c6eb0ba7472b6ee412
Guillaume Duhan <myhappyagency@gmail.com>
2019-12-21 23:19:45 +0100

    post.js deletePost

--- controllers/post.js
+++ controllers/post.js
@@ -21,3 +21,31 @@ exports.createPost = async (payload) => {
       return err
     })
 }
+
+exports.updatePost = async (payload) => {
+  return posts
+    .doc(payload.id)
+    .update(payload)
+    .then((post) => {
+      console.log(chalk.bold(chalk.blue('👍 Post updated: ', payload.id)))
+      return post
+    })
+    .catch((err) => {
+      console.log(chalk.bold(chalk.red('👿 Error on updating post: ' + err)))
+      return err
+    })
+}
+
+exports.deletePost = async (payload) => {
+  return posts
+    .doc(payload.id)
+    .delete()
+    .then((post) => {
+      console.log(chalk.bold(chalk.green('👍 Post deleted: ', payload.id)))
+      return post
+    })
+    .catch((err) => {
+      console.log(chalk.bold(chalk.red('👿 Error on deleting post: ' + err)))
+      return err
+    })
+}
