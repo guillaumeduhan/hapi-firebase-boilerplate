8a0309fa3253675db419127f9452b4feba763796
Guillaume Duhan <myhappyagency@gmail.com>
2019-12-25 10:56:37 +0100

    post instead of document

---
+++ routes/post.js
@@ -0,0 +1,34 @@
+/*
+ * Documents routes
+ */
+
+'use strict'
+
+const postsController = require('../controllers/post.js')
+
+module.exports = [
+  {
+    method: 'POST',
+    path: '/document/create',
+    handler: async (req) => {
+      const newPost = await postsController.create(req.payload)
+      return newPost
+    },
+  },
+  {
+    method: 'PUT',
+    path: '/document/update',
+    handler: async (req) => {
+      const updatedPost = await postsController.update(req.payload)
+      return updatedPost
+    },
+  },
+  {
+    method: 'POST',
+    path: '/document/delete',
+    handler: async (req) => {
+      const deletedPost = await postsController.delete(req.payload)
+      return deletedPost
+    },
+  },
+]
