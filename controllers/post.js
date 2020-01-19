04182802f27617c6ed87e3c5431770017d9d2499
Guillaume Duhan <myhappyagency@gmail.com>
2020-01-19 11:30:17 +0100

    post update

--- controllers/post.js
+++ controllers/post.js
@@ -5,19 +5,15 @@
 'use strict'
 
 const admin = require('firebase-admin')
-const chalk = require('chalk')
 const store = admin.firestore()
 const posts = store.collection('posts')
-
 exports.create = async (payload) => {
   return posts
     .add(payload)
     .then((post) => {
-      console.log(chalk.bold(chalk.green('👍 New post added: ', post.id)))
       return post
     })
     .catch((err) => {
-      console.log(chalk.bold(chalk.red('👿 Error on post: ' + err)))
       return err
     })
 }
@@ -27,11 +23,9 @@ exports.update = async (payload) => {
     .doc(payload.id)
     .update(payload)
     .then((post) => {
-      console.log(chalk.bold(chalk.blue('👍 post updated: ', payload.id)))
       return post
     })
     .catch((err) => {
-      console.log(chalk.bold(chalk.red('👿 Error on updating post: ' + err)))
       return err
     })
 }
@@ -41,11 +35,9 @@ exports.delete = async (payload) => {
     .doc(payload.id)
     .delete()
     .then((post) => {
-      console.log(chalk.bold(chalk.green('👍 post deleted: ', payload.id)))
       return post
     })
     .catch((err) => {
-      console.log(chalk.bold(chalk.red('👿 Error on deleting post: ' + err)))
       return err
     })
 }
